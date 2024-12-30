ed272945270b56f51259949ef0c62b3d33eebf20
Hunter Hogan <hunterhogan@users.noreply.github.com>
2024-12-30 17:09:46 -0500

    Update module docstring for clarity and accuracy

--- mapFolding/__init__.py
+++ mapFolding/__init__.py
@@ -1,5 +1,5 @@
 """
-Python-JAX implementation of 
+Python implementation of 
 W. F. Lunnon, Multi-dimensional map-folding, "The Computer Journal", Volume 14, Issue 1, 1971, Pages 75-80, https://doi.org/10.1093/comjnl/14.1.75
 """
 # HEY! 
