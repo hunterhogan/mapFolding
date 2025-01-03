46bb0bdd69ada2874130c00717848e25f172f0d5
Hunter Hogan <hunterhogan@users.noreply.github.com>
2025-01-03 18:17:00 -0500

    Refactor index access in foldings function to use tuple notation for clarity

--- mapFolding/count.py
+++ mapFolding/count.py
@@ -48,11 +48,11 @@ def foldings(listDimensions: List[int]):
         cumulativeProduct[dimension1ndex] = cumulativeProduct[dimension1ndex - 1] * listDimensions[dimension1ndex - 1]
 
     """Step 2: for each dimension, create a coordinate system """
-    """coordinateSystem[dimension1ndex][leaf1ndex] holds the dimension1ndex-th coordinate of leaf leaf1ndex"""
+    """coordinateSystem[dimension1ndex, leaf1ndex] holds the dimension1ndex-th coordinate of leaf leaf1ndex"""
     coordinateSystem = numpy.zeros((dimensionsTotal + 1, leavesTotal + 1), dtype=dtypeDefault)
     for dimension1ndex in range(1, dimensionsTotal + 1):
         for leaf1ndex in range(1, leavesTotal + 1):
-            coordinateSystem[dimension1ndex][leaf1ndex] = ((leaf1ndex - 1) // cumulativeProduct[dimension1ndex - 1]) % listDimensions[dimension1ndex - 1] + 1
+            coordinateSystem[dimension1ndex, leaf1ndex] = ((leaf1ndex - 1) // cumulativeProduct[dimension1ndex - 1]) % listDimensions[dimension1ndex - 1] + 1
 
     """Step 3: create a huge empty connection graph"""
     connectionGraph = numpy.zeros((dimensionsTotal + 1, leavesTotal + 1, leavesTotal + 1), dtype=dtypeDefault)
@@ -61,16 +61,14 @@ def foldings(listDimensions: List[int]):
     for dimension1ndex in range(1, dimensionsTotal + 1):
         for activeLeaf1ndex in range(1, leavesTotal + 1):
             for leaf1ndexConnectee in range(1, activeLeaf1ndex + 1):
-                if (coordinateSystem[dimension1ndex][activeLeaf1ndex] & 1) == (coordinateSystem[dimension1ndex][leaf1ndexConnectee] & 1):
-                    if coordinateSystem[dimension1ndex][leaf1ndexConnectee] == 1:
-                        connectionGraph[dimension1ndex][activeLeaf1ndex][leaf1ndexConnectee] = leaf1ndexConnectee
-                    else:
-                        connectionGraph[dimension1ndex][activeLeaf1ndex][leaf1ndexConnectee] = leaf1ndexConnectee - cumulativeProduct[dimension1ndex - 1]
-                else: 
-                    if coordinateSystem[dimension1ndex][leaf1ndexConnectee] == listDimensions[dimension1ndex - 1] or leaf1ndexConnectee + cumulativeProduct[dimension1ndex - 1] > activeLeaf1ndex:
-                        connectionGraph[dimension1ndex][activeLeaf1ndex][leaf1ndexConnectee] = leaf1ndexConnectee
-                    else:
-                        connectionGraph[dimension1ndex][activeLeaf1ndex][leaf1ndexConnectee] = leaf1ndexConnectee + cumulativeProduct[dimension1ndex - 1]
+                connectionGraph[dimension1ndex, activeLeaf1ndex, leaf1ndexConnectee] = (0 if leaf1ndexConnectee == 0
+                                else ((leaf1ndexConnectee if coordinateSystem[dimension1ndex, leaf1ndexConnectee] == 1
+                                            else leaf1ndexConnectee - cumulativeProduct[dimension1ndex - 1])
+                                    if (coordinateSystem[dimension1ndex, activeLeaf1ndex] & 1) == (coordinateSystem[dimension1ndex, leaf1ndexConnectee] & 1)
+                                    else (leaf1ndexConnectee if coordinateSystem[dimension1ndex, leaf1ndexConnectee] == listDimensions[dimension1ndex-1] 
+                                            or leaf1ndexConnectee + cumulativeProduct[dimension1ndex - 1] > activeLeaf1ndex
+                                            else leaf1ndexConnectee + cumulativeProduct[dimension1ndex - 1])))
+
 
     track = numpy.zeros((4, leavesTotal + 1), dtype=dtypeDefault)
 
@@ -84,29 +82,29 @@ def foldings(listDimensions: List[int]):
     activeGap1ndex = integerSmall(0)
 
     while activeLeaf1ndex > 0:
-        if activeLeaf1ndex <= 1 or track[leafBelow][0] == 1:
+        if activeLeaf1ndex <= 1 or track[leafBelow, 0] == 1:
             if activeLeaf1ndex > leavesTotal:                
                 foldingsTotal += leavesTotal
             else:
                 dimensionsUnconstrained = integerSmall(0)
                 """Track possible gaps for activeLeaf1ndex in each section"""
-                gap1ndexLowerBound = track[gapRangeStart][activeLeaf1ndex - 1]
+                gap1ndexLowerBound = track[gapRangeStart, activeLeaf1ndex - 1]
                 """Reset gap index"""
                 activeGap1ndex = gap1ndexLowerBound
 
                 """Count possible gaps for activeLeaf1ndex in each section"""
                 dimension1ndex = integerSmall(1)
                 while dimension1ndex <= dimensionsTotal:
-                    if connectionGraph[dimension1ndex][activeLeaf1ndex][activeLeaf1ndex] == activeLeaf1ndex:
+                    if connectionGraph[dimension1ndex, activeLeaf1ndex, activeLeaf1ndex] == activeLeaf1ndex:
                         dimensionsUnconstrained += 1
                     else:
-                        leaf1ndexConnectee = connectionGraph[dimension1ndex][activeLeaf1ndex][activeLeaf1ndex]
+                        leaf1ndexConnectee = connectionGraph[dimension1ndex, activeLeaf1ndex, activeLeaf1ndex]
                         while leaf1ndexConnectee != activeLeaf1ndex:
                             potentialGaps[gap1ndexLowerBound] = leaf1ndexConnectee
-                            if track[countDimensionsGapped][leaf1ndexConnectee] == 0:
+                            if track[countDimensionsGapped, leaf1ndexConnectee] == 0:
                                 gap1ndexLowerBound += 1
-                            track[countDimensionsGapped][leaf1ndexConnectee] += 1
-                            leaf1ndexConnectee = connectionGraph[dimension1ndex][activeLeaf1ndex][track[leafBelow][leaf1ndexConnectee]]
+                            track[countDimensionsGapped, leaf1ndexConnectee] += 1
+                            leaf1ndexConnectee = connectionGraph[dimension1ndex, activeLeaf1ndex, track[leafBelow, leaf1ndexConnectee]]
                     dimension1ndex += 1
 
                 """If activeLeaf1ndex is unconstrained in all sections, it can be inserted anywhere"""
@@ -121,27 +119,27 @@ def foldings(listDimensions: List[int]):
                 indexMiniGap = activeGap1ndex
                 while indexMiniGap < gap1ndexLowerBound:
                     potentialGaps[activeGap1ndex] = potentialGaps[indexMiniGap]
-                    if track[countDimensionsGapped][potentialGaps[indexMiniGap]] == dimensionsTotal - dimensionsUnconstrained:
+                    if track[countDimensionsGapped, potentialGaps[indexMiniGap]] == dimensionsTotal - dimensionsUnconstrained:
                         activeGap1ndex += 1
                     """Reset track[countDimensionsGapped] for next iteration"""
-                    track[countDimensionsGapped][potentialGaps[indexMiniGap]] = 0
+                    track[countDimensionsGapped, potentialGaps[indexMiniGap]] = 0
                     indexMiniGap += 1
 
         """Recursive backtracking steps"""
-        while activeLeaf1ndex > 0 and activeGap1ndex == track[gapRangeStart][activeLeaf1ndex - 1]:
+        while activeLeaf1ndex > 0 and activeGap1ndex == track[gapRangeStart, activeLeaf1ndex - 1]:
             activeLeaf1ndex -= 1
-            track[leafBelow][track[leafAbove][activeLeaf1ndex]] = track[leafBelow][activeLeaf1ndex]
-            track[leafAbove][track[leafBelow][activeLeaf1ndex]] = track[leafAbove][activeLeaf1ndex]
+            track[leafBelow, track[leafAbove, activeLeaf1ndex]] = track[leafBelow, activeLeaf1ndex]
+            track[leafAbove, track[leafBelow, activeLeaf1ndex]] = track[leafAbove, activeLeaf1ndex]
 
         """Place leaf in valid position"""
         if activeLeaf1ndex > 0:
             activeGap1ndex -= 1
-            track[leafAbove][activeLeaf1ndex] = potentialGaps[activeGap1ndex]
-            track[leafBelow][activeLeaf1ndex] = track[leafBelow][track[leafAbove][activeLeaf1ndex]]
-            track[leafBelow][track[leafAbove][activeLeaf1ndex]] = activeLeaf1ndex
-            track[leafAbove][track[leafBelow][activeLeaf1ndex]] = activeLeaf1ndex
+            track[leafAbove, activeLeaf1ndex] = potentialGaps[activeGap1ndex]
+            track[leafBelow, activeLeaf1ndex] = track[leafBelow, track[leafAbove, activeLeaf1ndex]]
+            track[leafBelow, track[leafAbove, activeLeaf1ndex]] = activeLeaf1ndex
+            track[leafAbove, track[leafBelow, activeLeaf1ndex]] = activeLeaf1ndex
             """Save current gap index"""
-            track[gapRangeStart][activeLeaf1ndex] = activeGap1ndex
+            track[gapRangeStart, activeLeaf1ndex] = activeGap1ndex
             """Move to next leaf"""
             activeLeaf1ndex += 1
     return int(foldingsTotal)
--- mapFolding/reference/lunnan.py
+++ mapFolding/reference/lunnan.py
@@ -3,10 +3,18 @@ An unnecessarily literal translation of the original Atlas Autocode code by W. F
 W. F. Lunnon, Multi-dimensional map-folding, The Computer Journal, Volume 14, Issue 1, 1971, Pages 75-80, https://doi.org/10.1093/comjnl/14.1.75
 """
 def foldings(p, job=None):
+    """An unnecessarily literal translation of the original Atlas Autocode code."""
     p = list(p)
-    p.append(None) # NOTE mimics Atlas `array` type
-    p.insert(0, None) # NOTE mimics Atlas `array` type
+    p.append(None)  # NOTE mimics Atlas `array` type
+    p.insert(0, None)  # NOTE mimics Atlas `array` type
 
+    if job is None:
+        global f
+        f = 0
+        def job(A, B):
+            global f
+            f = f + 1
+        return foldings(p, job)
     # perform job (A, B) on each folding of a p[1] x ... x p[d] map,
     # where A and B are the above and below vectors. p[d + 1] < 0 terminates p;
 
@@ -29,136 +37,70 @@ def foldings(p, job=None):
 
     # d dimensions and n leaves;
 
-    f = 0 # TODO
+    # A: list[int] = [None] * (n + 1) # type: ignore
+    # B: list[int] = [None] * (n + 1) # type: ignore
+    # count: list[int] = [None] * (n + 1) # type: ignore
+    # gapter: list[int] = [None] * (n + 1) # type: ignore
+    # gap: list[int] = [None] * (n * n + 1) # type: ignore
+    A: list[int] = [0] * (n + 1) # type: ignore
+    B: list[int] = [0] * (n + 1) # type: ignore
+    count: list[int] = [0] * (n + 1) # type: ignore
+    gapter: list[int] = [0] * (n + 1) # type: ignore
+    gap: list[int] = [0] * (n * n + 1) # type: ignore
 
-    def job(A, B): # TODO here?
-        global A, B, count, gapter, gap, D, g
-        nonlocal f
-        f = f + 1
+    # B[m] is the leaf below leaf m in the current folding,
+    # A[m] the leaf above. count[m] is the no. of sections in which
+    # there is a gap for the new leaf l below leaf m,
+    # gap[gapter[l - 1] + j] is the j-th (possible or actual) gap for leaf l,
+    # and later gap[gapter[l]] is the gap where leaf l is currently inserted;
 
-        A: list[int] = [None] * (n + 1) # type: ignore
-        B: list[int] = [None] * (n + 1) # type: ignore
-        count: list[int] = [None] * (n + 1) # type: ignore
-        gapter = [0] * (n + 1)
-        gap = [0] * (n * n + 1)
+    P: list[int] = [0] * (d + 1) # type: ignore
+    C: list[list[int]] = [[0] * (n + 1) for dimension1 in range(d + 1)] # type: ignore
+    # D: list[list[list[int]]] = [[[None] * (n + 1) for dimension2 in range(n + 1)] for dimension1 in range(d + 1)] # type: ignore
+    D: list[list[list[int]]] = [[[0] * (n + 1) for dimension2 in range(n + 1)] for dimension1 in range(d + 1)]
 
-        # B[m] is the leaf below leaf m in the current folding,
-        # A[m] the leaf above. count[m] is the no. of sections in which
-        # there is a gap for the new leaf l below leaf m,
-        # gap[gapter[l - 1] + j] is the j-th (possible or actual) gap for leaf l,
-        # and later gap[gapter[l]] is the gap where leaf l is currently inserted
+    P[0] = 1
+    for i in range(1, d + 1):
+        P[i] = P[i - 1] * p[i]
 
-        P: list[int] = [None] * (d + 1) # type: ignore
-        C: list[list[int]] = [[None] * (n + 1) for dimension1 in range(d + 1)] # type: ignore
-        # D: list[list[list[int]]] = [[[None] * (n + 1) for dimension2 in range(n + 1)] for dimension1 in range(d + 1)] # type: ignore
-        D: list[list[list[int]]] = [[[0] * (n + 1) for dimension2 in range(n + 1)] for dimension1 in range(d + 1)]
+    for i in range(1, d + 1):
+        for m in range(1, n + 1):
+            C[i][m] = ((m - 1) // P[i - 1]) - ((m - 1) // P[i]) * p[i] + 1
 
-        P[0] = 1
-        for i in range(1, d + 1):
-            P[i] = P[i - 1] * p[i]
-
-        for i in range(1, d + 1):
-            for m in range(1, n + 1):
-                C[i][m] = ((m - 1) // P[i - 1]) - ((m - 1) // P[i]) * p[i] + 1
-
-        for i in range(1, d + 1):
-            for l in range(1, n + 1):
-                for m in range(1, l + 1):
-                    D[i][l][m] = (0 if m == 0
+    for i in range(1, d + 1):
+        for l in range(1, n + 1):
+            for m in range(1, l + 1):
+                D[i][l][m] = (0 if m == 0
+                                else
+                                        ((m if C[i][m] == 1
+                                            else m - P[i - 1])
+                                    if C[i][l] - C[i][m] == (C[i][l] - C[i][m]) // 2 * 2
                                     else
-                                            ((m if C[i][m] == 1
-                                                else m - P[i - 1])
-                                        if C[i][l] - C[i][m] == (C[i][l] - C[i][m]) // 2 * 2
-                                        else
-                                            (m if C[i][m] == p[i] or m + P[i - 1] > l
-                                                else m + P[i - 1])))
-        # P[i] = p[1] x ... x p[i], C[i][m] = i-th co-ordinate of leaf m,
-        # D[i][l][m] = leaf connected to m in section i when inserting l;
+                                        (m if C[i][m] == p[i] or m + P[i - 1] > l
+                                            else m + P[i - 1])))
+    # P[i] = p[1] x ... x p[i], C[i][m] = i-th co-ordinate of leaf m,
+    # D[i][l][m] = leaf connected to m in section i when inserting l;
 
-        for m in range(n + 1):
-            count[m] = 0
+    for m in range(n + 1):
+        count[m] = 0
 
-        A[0], B[0], g, l = 0, 0, 0, 0
+    A[0], B[0], g, l = 0, 0, 0, 0
 
-        # goto entry;
-        # kick off with null folding
-
-    def down():
-        nonlocal g, gap
-        dd = 0
-        gg = gapter[l - 1]
-        g = gg
-        # dd is the no. of sections in which l is unconstrained,
-        # gg the no. of possible and g the no. of actual gaps for l, + gapter[l - 1]
-
-        # find the possible gaps for leaf l in each section,
-        # then discard those not common to all. All possible if dd = d
-        for i in range(1, d + 1):
-            if D[i][l][l] == l:
-                dd = dd + 1
+    state = 'entry'
+    while True:
+        if state == 'entry':
+            gapter[l] = g
+            l = l + 1
+            if l <= n:
+                state = 'down'
+                continue
             else:
-                m = D[i][l][l]
-                while m != l:
-                    gap[gg] = m
-                    if count[m] == 0:
-                        gg = gg + 1
-                    count[m] = count[m] + 1
-                    m = D[i][l][B[m]]
+                job(A, B)
+                state = 'up'
+                continue
 
-        if dd == d:
-            for m in range(l):
-                gap[gg] = m
-                gg = gg + 1
-
-        for j in range(g, gg):
-            gap[g] = gap[j]
-            if count[gap[j]] == d - dd:
-                g = g + 1
-            count[gap[j]] = 0
-
-        # for each gap insert leaf l, call self recursively,
-            # remove leaf l;
-
-        along()
-
-    def along():
-        nonlocal g, A, B
-        if g == gapter[l - 1]:
-            up()
-        g = g - 1
-        A[l] = gap[g]
-        B[l] = B[A[l]]
-        B[A[l]] = l
-        A[B[l]] = l
-        entry()
-
-    def entry():
-        nonlocal gapter, l
-        gapter[l] = g
-        l = l + 1
-        if l <= n:
-            down()
-        else:
-            job(A, B) # type: ignore
-
-    def up():
-        nonlocal l, A, B
-        l = l - 1
-        B[A[l]] = B[l]
-        A[B[l]] = A[l]
-        if l > 0:
-            along()
-        # TODO ?
-        # else:
-        #     return
-
-    entry() # TODO: flow control is different
-
-    while l > 0:
-        if l > n:
-            f = f + 1
-        else:
-            dd = 0                  # down START NOTE
+        elif state == 'down':
+            dd = 0
             gg = gapter[l - 1]
             g = gg
             for i in range(1, d + 1):
@@ -182,19 +124,30 @@ def foldings(p, job=None):
                 gap[g] = gap[j]
                 if count[gap[j]] == d - dd:
                     g = g + 1
-                count[gap[j]] = 0   # down END NOTE
+                count[gap[j]] = 0
+            state = 'along'
+            continue
 
-        while l > 0 and g == gapter[l - 1]:
-            l = l - 1       # up
-            B[A[l]] = B[l]  # up
-            A[B[l]] = A[l]  # up
+        elif state == 'along':
+            if g == gapter[l - 1]:
+                state = 'up'
+                continue
+            g = g - 1
+            A[l] = gap[g]
+            B[l] = B[A[l]]
+            B[A[l]] = l
+            A[B[l]] = l
+            state = 'entry'
+            continue
 
-        if l > 0:
-            g = g - 1       # along
-            A[l] = gap[g]   # along
-            B[l] = B[A[l]]  # along
-            B[A[l]] = l     # along
-            A[B[l]] = l     # along
-            gapter[l] = g   # entry
-            l = l + 1       # entry
-    return f
+        elif state == 'up':
+            l = l - 1
+            B[A[l]] = B[l]
+            A[B[l]] = A[l]
+            if l > 0:
+                state = 'along'
+                continue
+            else:
+                break
+
+    return f #if job.__closure__ else None
