6a4bd8c93de6e4383ddabcb1e030ce7ae9c517dd
Hunter Hogan <hunterhogan@users.noreply.github.com>
2025-01-05 20:25:09 -0500

    Lola baseline

--- mapFolding/__init__.py
+++ mapFolding/__init__.py
@@ -1,5 +1,5 @@
 from .beDRY import getLeavesTotal, parseListDimensions, validateListDimensions, validateTaskDivisions, outfitFoldings, makeConnectionGraph
-from .lolaLego import countFolds
+from .lola import countFolds
 from .oeis import oeisSequence_aOFn, getOEISids, clearOEIScache
 
 __all__ = [
