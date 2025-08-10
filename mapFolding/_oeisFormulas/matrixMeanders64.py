0c3dca569caab4ddfd762a39dfe0f963227c5a8c
Hunter Hogan <hunterhogan@users.noreply.github.com>
2025-08-10 02:19:03 -0500

    basic ndarray

--- mapFolding/_oeisFormulas/matrixMeanders64.py
+++ mapFolding/_oeisFormulas/matrixMeanders64.py
@@ -1,13 +1,12 @@
 import numpy
 
+distinctCrossings = int(0)
+bifurcationAlpha = int(1)
+bifurcationZulu = int(2)
+
 """TODO temporary notes:
 
 Flow:
-- startingCurveLocations to ndarray, `arrayCurveLocations`
-	- curveLocations
-	- distinctCrossings
-	- bifurcationAlpha
-	- bifurcationZulu
 - mask construction
 	- Z0Z_simpleBridges
 	- bifurcationAlphaCurves
@@ -23,38 +22,45 @@ Flow:
 """
 
 def count(bridges: int, startingCurveLocations: dict[int, int]) -> int:
-
 	dictionaryCurveLocations: dict[int, int] = {}
 
 	while bridges > 0:
 		bridges -= 1
-		curveLocationsMAXIMUM = 1 << (2 * bridges + 4)
-		bifurcationAlphaLocator = int('01' * ((curveLocationsMAXIMUM.bit_length() + 1) // 2), 2)
-		bifurcationZuluLocator = bifurcationAlphaLocator << 1
 
-		for curveLocations, distinctCrossings in startingCurveLocations.items():
-			bifurcationAlpha = (curveLocations & bifurcationAlphaLocator)
-			bifurcationZulu = (curveLocations & bifurcationZuluLocator) >> 1
+		curveLocationsMAXIMUM = numpy.uint64(1) << numpy.uint64(2 * bridges + 4)
+		bifurcationAlphaLocator = (
+			(1 << 2 * ((int(curveLocationsMAXIMUM).bit_length() + 1) // 2)
+			) - 1
+		) // 3
+		bifurcationZuluLocator  = bifurcationAlphaLocator << numpy.uint64(1)
 
-			bifurcationAlphaHasCurves = bifurcationAlpha != 1
-			bifurcationZuluHasCurves = bifurcationZulu != 1
+		arrayKeys = numpy.fromiter(startingCurveLocations.keys(), dtype=numpy.uint64)
+		arrayCurveLocations = numpy.column_stack((
+			numpy.fromiter(startingCurveLocations.values(), dtype=numpy.uint64),
+			arrayKeys & bifurcationAlphaLocator,
+			(arrayKeys & bifurcationZuluLocator) >> numpy.uint64(1),
+		))
+
+		for index in range(len(arrayCurveLocations)):
+			bifurcationAlphaHasCurves = arrayCurveLocations[index, bifurcationAlpha] != 1
+			bifurcationZuluHasCurves = arrayCurveLocations[index, bifurcationZulu] != 1
 
 			# Z0Z_simpleBridges
-			curveLocationAnalysis = ((bifurcationAlpha | (bifurcationZulu << 1)) << 2) | 3
+			curveLocationAnalysis = ((arrayCurveLocations[index, bifurcationAlpha] | (arrayCurveLocations[index, bifurcationZulu] << 1)) << 2) | 3
 			if curveLocationAnalysis < curveLocationsMAXIMUM:
-				dictionaryCurveLocations[curveLocationAnalysis] = dictionaryCurveLocations.get(curveLocationAnalysis, 0) + distinctCrossings
+				dictionaryCurveLocations[curveLocationAnalysis] = dictionaryCurveLocations.get(curveLocationAnalysis, 0) + arrayCurveLocations[index, distinctCrossings]
 
 			# bifurcationAlphaCurves
 			if bifurcationAlphaHasCurves:
-				curveLocationAnalysis = (bifurcationAlphaShiftRight2 := bifurcationAlpha >> 2) | (bifurcationZulu << 3) | ((bifurcationAlphaIsEven := 1 - (bifurcationAlpha & 0b1)) << 1)
+				curveLocationAnalysis = (bifurcationAlphaShiftRight2 := arrayCurveLocations[index, bifurcationAlpha] >> 2) | (arrayCurveLocations[index, bifurcationZulu] << 3) | ((bifurcationAlphaIsEven := 1 - (arrayCurveLocations[index, bifurcationAlpha] & 0b1)) << 1)
 				if curveLocationAnalysis < curveLocationsMAXIMUM:
-					dictionaryCurveLocations[curveLocationAnalysis] = dictionaryCurveLocations.get(curveLocationAnalysis, 0) + distinctCrossings
+					dictionaryCurveLocations[curveLocationAnalysis] = dictionaryCurveLocations.get(curveLocationAnalysis, 0) + arrayCurveLocations[index, distinctCrossings]
 
 			# bifurcationZuluCurves
 			if bifurcationZuluHasCurves:
-				curveLocationAnalysis = (bifurcationZulu >> 1) | (bifurcationAlpha << 2) | (bifurcationZuluIsEven := not (bifurcationZulu & 1))
+				curveLocationAnalysis = (arrayCurveLocations[index, bifurcationZulu] >> 1) | (arrayCurveLocations[index, bifurcationAlpha] << 2) | (bifurcationZuluIsEven := not (arrayCurveLocations[index, bifurcationZulu] & 1))
 				if curveLocationAnalysis < curveLocationsMAXIMUM:
-					dictionaryCurveLocations[curveLocationAnalysis] = dictionaryCurveLocations.get(curveLocationAnalysis, 0) + distinctCrossings
+					dictionaryCurveLocations[curveLocationAnalysis] = dictionaryCurveLocations.get(curveLocationAnalysis, 0) + arrayCurveLocations[index, distinctCrossings]
 
 			# Z0Z_alignedBridges
 			if bifurcationAlphaHasCurves and bifurcationZuluHasCurves:
@@ -68,17 +74,17 @@ def count(bridges: int, startingCurveLocations: dict[int, int]) -> int:
 					if bifurcationsCanBePairedTogether == 1:
 						while findUnpaired_0b1 >= 0:
 							XOrHere2makePair <<= 2
-							findUnpaired_0b1 += 1 if (bifurcationAlpha & XOrHere2makePair) == 0 else -1
-						bifurcationAlphaShiftRight2 = (bifurcationAlpha ^ XOrHere2makePair) >> 2
+							findUnpaired_0b1 += 1 if (arrayCurveLocations[index, bifurcationAlpha] & XOrHere2makePair) == 0 else -1
+						bifurcationAlphaShiftRight2 = (arrayCurveLocations[index, bifurcationAlpha] ^ XOrHere2makePair) >> 2
 					elif bifurcationsCanBePairedTogether == 2:
 						while findUnpaired_0b1 >= 0:
 							XOrHere2makePair <<= 2
-							findUnpaired_0b1 += 1 if (bifurcationZulu & XOrHere2makePair) == 0 else -1
-						bifurcationZulu ^= XOrHere2makePair
+							findUnpaired_0b1 += 1 if (arrayCurveLocations[index, bifurcationZulu] & XOrHere2makePair) == 0 else -1
+						arrayCurveLocations[index, bifurcationZulu] ^= XOrHere2makePair
 
-					curveLocationAnalysis = ((bifurcationZulu >> 2) << 1) | bifurcationAlphaShiftRight2 # pyright: ignore[reportPossiblyUnboundVariable]
+					curveLocationAnalysis = ((arrayCurveLocations[index, bifurcationZulu] >> 2) << 1) | bifurcationAlphaShiftRight2 # pyright: ignore[reportPossiblyUnboundVariable]
 					if curveLocationAnalysis < curveLocationsMAXIMUM:
-						dictionaryCurveLocations[curveLocationAnalysis] = dictionaryCurveLocations.get(curveLocationAnalysis, 0) + distinctCrossings
+						dictionaryCurveLocations[curveLocationAnalysis] = dictionaryCurveLocations.get(curveLocationAnalysis, 0) + arrayCurveLocations[index, distinctCrossings]
 
 		startingCurveLocations.clear()
 		startingCurveLocations, dictionaryCurveLocations = dictionaryCurveLocations, startingCurveLocations
